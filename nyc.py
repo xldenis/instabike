f2c1d188cf44c31d34fe64f7ca89737c3548dcd5
Xavier Denis <xldenis@gmail.com>
2014-12-08 15:51:15 -0500

    Update neural net

--- nyc.py
+++ nyc.py
@@ -6,26 +6,34 @@ import pylab as pl
 import numpy as np
 from matplotlib.dates import YearLocator, MonthLocator, DateFormatter
 
+from pandas import DataFrame
 
 ###############################################################################
 # Grab pickled data 
 ###############################################################################
 
+def load_simple():
+  srtd = pickle.load(open('sorted.pk', 'rb'))
+  fnl = pickle.load(open('fnl.pk', 'rb'))
 
-srtd = pickle.load(open('sorted.pk', 'rb'))
-fnl = pickle.load(open('fnl.pk', 'rb'))
+  start = datetime(1995, 1, 1)
+  s = int(start.strftime("%s")) / 86400
 
-start = datetime(1995, 1, 1)
-s = int(start.strftime("%s")) / 86400
+  def convDate(date):
+  	t = int(date.strftime("%s")) / 86400
+  	return t - s
 
-def convDate(date):
-	t = int(date.strftime("%s")) / 86400
-	return t - s
-
-end = datetime(2009, 12, 31)
-e = convDate(end)
-all_days = range(1, e)
+  end = datetime(2009, 12, 31)
+  e = convDate(end)
+  all_days = range(1, e)
 
 
-train_set = fnl[:4930]
-test_set = fnl[4930:]
+  train_set = fnl[:4930]
+  test_set = fnl[4930:] 
+  return fnl
+
+def load_full():
+  fnl = pickle.load(open('weather_fnl','rb'))
+  data = DataFrame(list(DataFrame(fnl)[0]))
+  weather = DataFrame(list(DataFrame(fnl)[1]))
+  return [data, weather]
--- weather_neural.py
+++ weather_neural.py
@@ -6,32 +6,37 @@ from pybrain.structure import FullConnection
 import pandas
 from collections import deque
 from pandas.stats.moments import rolling_mean
-
+from itertools import chain
 from nyc import *
 from avr import avr_p
 
 import matplotlib.pyplot as plt
 
-frame, _wea = load_full()
+frame, weather = load_full()
+
 # frame = pandas.DataFrame(fnl)
 # frame = (frame - frame.mean()) / (frame.max() - frame.min())
 frame = (frame - frame.mean()) / (frame.var())
 for k in [30]:
   for i in frame.columns:
 
-    fnn = buildNetwork(k,10,1)
+    fnn = buildNetwork(2*k,10,1)
 
-    DS = SupervisedDataSet(k, 1)
-    dta = frame[i][:5000]
+    DS = SupervisedDataSet(2*k, 1)
+    dta = frame[i][:int(len(frame)*0.8)]
+    wea = weather[0][:int(len(frame)*0.8)]
+
     for j in xrange(0, len(dta) - (k+1)):
-      DS.appendLinked(dta[j:j+k], [dta[j+k+1]])
+      DS.appendLinked(list(chain(*zip(dta[j:j+k], wea[j:j+k]))), [dta[j+k+1]])
 
-    test = frame[i][5000:]
-    testDS = SupervisedDataSet(k, 1)
+    test = frame[i][int(len(frame)*0.8):]
+    wea = weather[0][int(len(frame)*0.8):]
+    testDS = SupervisedDataSet(2*k, 1)
+
     for j in xrange(0, len(test) - (k+1)):
-      testDS.appendLinked(test[j:j+k], [test[5000+i+k+1]])
+      testDS.appendLinked(list(chain(*zip(test[j:j+k], wea[j:j+k]))), [test[5000+i+k+1]])
 
-    trainer = BackpropTrainer(fnn, dataset=DS, momentum=0.1, verbose=False, weightdecay=0.01)
+    trainer = BackpropTrainer(fnn, dataset=DS, momentum=0.1, verbose=True, weightdecay=0.01)
 
     # for ep in range(0, 5):
       # trainer.trainEpochs()
@@ -45,6 +50,6 @@ for k in [30]:
     plt.figure()
     ax = rolling_mean(test,30).plot()
     rolling_mean(res,30).plot(ax=ax, style='r--')
-      # print 'AVR SCORE %f trace %d k %d epo %d' % (avr_p(test[k+1:],res), i, k, 5*ep)
+    print 'AVR SCORE %f trace %d k %d' % (avr_p(test[k+1:],res), i, k)
 # ax = rolling_mean(frame.iloc[5000:],30).plot()  
 plt.show()
