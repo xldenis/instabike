c0955b3fc39e6db67c91f3760f450a74409833f6
Xavier Denis <xldenis@gmail.com>
2014-12-07 19:44:41 -0500

    Normalize NN for better performance

--- neural.py
+++ neural.py
@@ -4,49 +4,57 @@ from pybrain.datasets.supervised import SupervisedDataSet
 from pybrain.structure import FullConnection
 import pandas
 from collections import deque
+from pandas.stats.moments import rolling_mean
 
-import nyc
+from nyc import *
 
-fnn = FeedForwardNetwork()
+import matplotlib.pyplot as plt
+
+frame = pandas.DataFrame(fnl)
+frame = (frame - frame.mean()) / (frame.max() - frame.min())
 k = 30
-inLayer = LinearLayer(k)
-hiddenLayer = SigmoidLayer(10)
-outLayer = LinearLayer(1)
 
-fnn.addInputModule(inLayer)
-fnn.addModule(hiddenLayer)
-fnn.addOutputModule(outLayer)
+for i in [0,1,2]:
 
-in_to_hidden = FullConnection(inLayer, hiddenLayer)
-hidden_to_out = FullConnection(hiddenLayer, outLayer)
+  fnn = FeedForwardNetwork()
+  inLayer = LinearLayer(k)
+  hiddenLayer = SigmoidLayer(10)
+  outLayer = LinearLayer(1)
 
-fnn.addConnection(in_to_hidden)
-fnn.addConnection(hidden_to_out)
+  fnn.addInputModule(inLayer)
+  fnn.addModule(hiddenLayer)
+  fnn.addOutputModule(outLayer)
 
-fnn.sortModules()
+  in_to_hidden = FullConnection(inLayer, hiddenLayer)
+  hidden_to_out = FullConnection(hiddenLayer, outLayer)
 
-DS = SupervisedDataSet(k, 1)
-dta = pandas.DataFrame(fnl)[0][:5000]
-for i in xrange(0, len(dta) - (k+1)):
-  DS.appendLinked(dta[i:i+k], [dta[i+k+1]])
+  fnn.addConnection(in_to_hidden)
+  fnn.addConnection(hidden_to_out)
 
-test = pandas.DataFrame(fnl)[0][5000:]
-testDS = SupervisedDataSet(k, 1)
+  fnn.sortModules()
 
-for i in xrange(0, len(test) - (k+1)):
-  testDS.appendLinked(test[i:i+k], [test[5000+i+k+1]])
+  DS = SupervisedDataSet(k, 1)
+  dta = frame[i][:5000]
+  for j in xrange(0, len(dta) - (k+1)):
+    DS.appendLinked(dta[j:j+k], [dta[j+k+1]])
 
-trainer = BackpropTrainer(fnn, dataset=DS, momentum=0.1, verbose=True, weightdecay=0.01)
+  test = frame[i][5000:]
+  testDS = SupervisedDataSet(k, 1)
+  for j in xrange(0, len(test) - (k+1)):
+    testDS.appendLinked(test[j:j+k], [test[5000+i+k+1]])
 
-# trainer.trainEpochs(15)
+  trainer = BackpropTrainer(fnn, dataset=DS, momentum=0.1, verbose=True, weightdecay=0.01)
 
-trainer.trainUntilConvergence(maxEpochs=50)
+  # trainer.trainEpochs(15)
 
-res = fnn.activateOnDataset(testDS)
+  trainer.trainUntilConvergence(maxEpochs=5)
 
-res = pandas.DataFrame(res)
-res.index = test[k+1:].index
+  res = fnn.activateOnDataset(testDS)
 
-ax = rolling_mean(test,30).plot()
-rolling_mean(res,30).plot(ax=ax, style='r--')
+  res = pandas.DataFrame(res)
+  res.index = test[k+1:].index
+  plt.figure()
+  ax = rolling_mean(test,30).plot()
+  rolling_mean(res,30).plot(ax=ax, style='r--')
+ax = rolling_mean(frame.iloc[5000:],30).plot()  
 plt.show()
