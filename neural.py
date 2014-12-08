1c68534afd682f34f9bbc711cdee048414c7435c
Xavier Denis <xldenis@gmail.com>
2014-12-08 14:16:28 -0500

    Add hyper param tests

--- neural.py
+++ neural.py
@@ -1,4 +1,5 @@
-from pybrain.structure import LinearLayer, SigmoidLayer, FeedForwardNetwork
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure import SoftmaxLayer
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.datasets.supervised import SupervisedDataSet
 from pybrain.structure import FullConnection
@@ -14,50 +15,35 @@ import matplotlib.pyplot as plt
 frame = pandas.DataFrame(fnl)
 # frame = (frame - frame.mean()) / (frame.max() - frame.min())
 frame = (frame - frame.mean()) / (frame.var())
-k = 30
+for k in [1, 2, 7, 10, 30]:
+  for i in frame.columns:
 
-for i in frame.columns:
+    fnn = buildNetwork(k,10,1)
 
-  fnn = FeedForwardNetwork()
-  inLayer = LinearLayer(k)
-  hiddenLayer = SigmoidLayer(10)
-  outLayer = LinearLayer(1)
+    DS = SupervisedDataSet(k, 1)
+    dta = frame[i][:5000]
+    for j in xrange(0, len(dta) - (k+1)):
+      DS.appendLinked(dta[j:j+k], [dta[j+k+1]])
 
-  fnn.addInputModule(inLayer)
-  fnn.addModule(hiddenLayer)
-  fnn.addOutputModule(outLayer)
+    test = frame[i][5000:]
+    testDS = SupervisedDataSet(k, 1)
+    for j in xrange(0, len(test) - (k+1)):
+      testDS.appendLinked(test[j:j+k], [test[5000+i+k+1]])
 
-  in_to_hidden = FullConnection(inLayer, hiddenLayer)
-  hidden_to_out = FullConnection(hiddenLayer, outLayer)
+    trainer = BackpropTrainer(fnn, dataset=DS, momentum=0.1, verbose=False, weightdecay=0.01)
 
-  fnn.addConnection(in_to_hidden)
-  fnn.addConnection(hidden_to_out)
+    for ep in range(0, 5):
+      trainer.trainEpochs(5)
 
-  fnn.sortModules()
+      # trainer.trainUntilConvergence()
 
-  DS = SupervisedDataSet(k, 1)
-  dta = frame[i][:5000]
-  for j in xrange(0, len(dta) - (k+1)):
-    DS.appendLinked(dta[j:j+k], [dta[j+k+1]])
+      res = fnn.activateOnDataset(testDS)
 
-  test = frame[i][5000:]
-  testDS = SupervisedDataSet(k, 1)
-  for j in xrange(0, len(test) - (k+1)):
-    testDS.appendLinked(test[j:j+k], [test[5000+i+k+1]])
-
-  trainer = BackpropTrainer(fnn, dataset=DS, momentum=0.1, verbose=True, weightdecay=0.01)
-
-  # trainer.trainEpochs(15)
-
-  trainer.trainUntilConvergence(maxEpochs=10)
-
-  res = fnn.activateOnDataset(testDS)
-
-  res = pandas.DataFrame(res)
-  res.index = test[k+1:].index
-  plt.figure()
-  ax = rolling_mean(test,30).plot()
-  rolling_mean(res,30).plot(ax=ax, style='r--')
-  print 'AVR SCORE %f' %(avr_p(test[k+1:],res))
-ax = rolling_mean(frame.iloc[5000:],30).plot()  
-plt.show()
+      res = pandas.DataFrame(res)
+      res.index = test[k+1:].index
+      # plt.figure()
+      # ax = rolling_mean(test,30).plot()
+      # rolling_mean(res,30).plot(ax=ax, style='r--')
+      print 'AVR SCORE %f trace %d k %d epo %d' % (avr_p(test[k+1:],res), i, k, 5*ep)
+# ax = rolling_mean(frame.iloc[5000:],30).plot()  
+# plt.show()
