d84c15a78ebe9faf6d02b1912b9748ed9ac030b5
Deepanjan Roy <deepanjan.roy@mail.mcgill.ca>
2014-12-07 19:35:44 -0500

    Silly typo

--- avr.py
+++ avr.py
@@ -5,7 +5,7 @@ def avr(true_y, predicted_y):
     predicted_y = np.array(predicted_y)
 
     mean = true_y.mean()
-    diff = true_y - pred_y
+    diff = true_y - predicted_y
     diff_sq = diff ** 2
     denom = true_y - mean
     denom_sq = denom ** 2
